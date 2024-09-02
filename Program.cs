5c5b4660616ba4fa52f2b8165e49595c61fc47d7
Eduardo Thomas <eduardothomas64@gmail.com>
2024-09-02 16:20:51 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Raylib_cs;
+
+using static Raylib_cs.Raylib;
+
+namespace HelloWorld;
+
+class Program
+{
+    public static void Main()
+    {
+        InitWindow(800, 480, "Hello World");
+
+        while (!WindowShouldClose())
+        {
+            BeginDrawing();
+            ClearBackground(Color.White);
+
+            DrawText("Hello, world 22!", 12, 12, 20, Color.Black);
+
+            EndDrawing();
+        }
+
+        CloseWindow();
+    }
+}
+
+// namespace HelloWorld;
+//
+// class Program
+// {
+//     public static void Main()
+//     {
+//         Raylib.InitWindow(800, 480, "Hello World");
+//
+//         while (!Raylib.WindowShouldClose())
+//         {
+//             Raylib.BeginDrawing();
+//             Raylib.ClearBackground(Color.White);
+//
+//             Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+//
+//             Raylib.EndDrawing();
+//         }
+//
+//         Raylib.CloseWindow();
+//     }
+// }
